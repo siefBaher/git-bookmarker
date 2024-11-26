32edda6d920c624749adb1369d8d21ac7a2dca7c
siefBaher <seifbaher15@gmail.com>
2024-11-26 20:27:41 +0200

    project completed git add .

--- JavaScript/index.js
+++ JavaScript/index.js
@@ -1,6 +1,8 @@
 var SiteName = document.getElementById("SiteName");
 var SiteURL = document.getElementById("SiteURL");
 var submitBTN = document.querySelector(".btn-brown");
+var lightBoxCountaner = document.querySelector(".lightBoxCountaner");
+var exit = document.querySelector(".fa-x");
 var siteList;
 if (localStorage.getItem("storageData") == null) {
   siteList = [];
@@ -9,38 +11,26 @@ if (localStorage.getItem("storageData") == null) {
 }
 displaySites();
 function addSite() {
-    if(
-        SiteName.classList.contains("is-valid")&&
-        SiteURL.classList.contains("is-valid")
-    ){
-        var data = {
-            sName: SiteName.value,
-            sURL: SiteURL.value,
-          };
-          siteList.push(data);
-          localStorage.setItem("storageData", JSON.stringify(siteList));
-          clearInputs();
-          displaySites();
-    }else{
-        Swal.fire({
-            title: "Custom animation with Animate.css",
-            showClass: {
-              popup: `
-                animate__animated
-                animate__fadeInUp
-                animate__faster
-              `
-            },
-            hideClass: {
-              popup: `
-                animate__animated
-                animate__fadeOutDown
-                animate__faster
-              `
-            }
-          });
-        
-    }
+  if (
+    SiteName.classList.contains("is-valid") &&
+    SiteURL.classList.contains("is-valid")
+  ) {
+    var data = {
+      sName: SiteName.value,
+      sURL: SiteURL.value,
+    };
+    siteList.push(data);
+    localStorage.setItem("storageData", JSON.stringify(siteList));
+    clearInputs();
+    displaySites();
+  } else {
+    lightBoxCountaner.classList.remove("d-none");
+    exit.addEventListener("click",function(){
+    lightBoxCountaner.classList.add("d-none")
+    });
+    SiteName.nextElementSibling.classList.remove("d-none")   
+    SiteURL.nextElementSibling.classList.remove("d-none")   
+  }
 }
 submitBTN.addEventListener("click", function () {
   addSite();
@@ -48,8 +38,8 @@ submitBTN.addEventListener("click", function () {
 function clearInputs() {
   SiteName.value = null;
   SiteURL.value = null;
-  SiteName.classList.remove("is-valid")
-  SiteURL.classList.remove("is-valid")
+  SiteName.classList.remove("is-valid");
+  SiteURL.classList.remove("is-valid");
 }
 
 function displaySites() {
@@ -61,7 +51,7 @@ function displaySites() {
             <div class="col-2"><button class="btn btn-limgreen btn-sm p-2">
                 <a href="${
                   siteList[i].sURL
-                }" class="text-decoration-none text-white" target="_blank">
+                }" class="text-decoration-none text-white">
                     <i class="fa-solid fa-eye fa-sm"></i> <span>Visit</span>
                 </a>
             </button></div>
@@ -87,18 +77,20 @@ function toValidate(element) {
     element.classList.add("is-valid");
     element.classList.remove("is-invalid");
     element.classList.remove("inputs");
-  }else{
+    element.nextElementSibling.classList.add("d-none");
+  } else {
     element.classList.remove("is-valid");
     element.classList.add("is-invalid");
     element.classList.remove("inputs");
+    element.nextElementSibling.classList.remove("d-none");
   }
 }
 
 SiteName.addEventListener("input", function () {
   toValidate(this);
+;
+  
 });
 SiteURL.addEventListener("input", function () {
   toValidate(this);
 });
-
-submitBTN.
